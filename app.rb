0d2931cbf556a1c058d8039ab49c7f97f1ad499a
Nicholas Ng <nicholasykng@gmail.com>
2019-12-29 22:42:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,13 +28,13 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     case params[:operation]
     when "add"
-      @answer = @number1 + @number2
+      @answer = (@number1 + @number2).to_s
     when "subtract"
-      @answer = @number1 - @number2
+      @answer = (@number1 - @number2).to_s
     when "multiply"
-      @answer = @number1 * @number2
+      @answer = (@number1 * @number2).to_s
     when "divide"
-      @answer = @number1 / @number2
+      @answer = (@number1 / @number2).to_s
     end
   end
 
