a9dff6205ef202c4bba23a2d67f96fbd5f0f8dda
Nicholas Ng <nicholasykng@gmail.com>
2019-12-29 22:46:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reversename/:name' do
     @reversename = params[:name].reverse
-    "#{@reversename}"
   end
 
   get '/square/:number' do
